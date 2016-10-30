cf25ffc7c4771ceb06bbd3da30acc94cf22d2ab4
Richa3masa <signoremasa@gamil.com>
2016-10-30 19:12:40 +0900

    Change

--- helloworld.sh
+++ helloworld.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-read VERSION
-echo “$VERSION”
-echo "HelO333 World"
+read VERSION1
+read VERSION2
+sed -e s/$VERSION1/$VERSION2/g /Users/masayuki/git/practice/test1.xml > /Users/masayuki/git/practice/test1-1.xml
+cp /Users/masayuki/git/practice/test1-1.xml /Users/masayuki/git/practice/test1.xml  
+rm /Users/masayuki/git/practice/test1-1.xml
+sed -e s/$VERSION1/$VERSION2/g /Users/masayuki/git/practice/test2.plist > /Users/masayuki/git/practice/test2-1.plist
+cp /Users/masayuki/git/practice/test2-1.plist /Users/masayuki/git/practice/test2.plist   
+rm /Users/masayuki/git/practice/test2-1.plist
+
+
